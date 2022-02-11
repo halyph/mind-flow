350546660da9969a90e4bb292fee2a4f25bbf55e
Orest Ivasiv <halyph@gmail.com>
2022-02-11 02:59:38 +0100

    Updated readme-index.py script (#161)

--- readme-index.py
+++ readme-index.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-import os
 import glob
 from collections import defaultdict
 
 README_HEADER = """# Mind~Flow
 
-[[twitter](https://twitter.com/halyph)]  [[linkedin](https://www.linkedin.com/in/oivasiv/)]  [[bookshelf](misc/books.md)]
+[[twitter](https://twitter.com/halyph)]  [[linkedin](https://www.linkedin.com/in/oivasiv/)]  [[bookshelf](docs/misc/books.md)]
 
 _Another try of blogging or personal knowledge base_
 
-![logo.png](logo.png)
+![logo.png](docs/logo.png)
 """
 #-----------------------------------------------------------------------------------------
 
@@ -19,6 +18,14 @@ _Another try of blogging or personal knowledge base_
 year_format = 'YYYY'
 date_format = 'YYYY-MM-DD'
 INDEX_FILE = 'README.md'
+PREFIX = 'docs/blog/'
+
+def define_date_ranges():
+    start = len(PREFIX) + len(year_format) + len('/')
+    end = start + len(date_format)
+    return (start, end)
+
+date_start, date_end = define_date_ranges()
 
 def read_line(file_name):
     with open(file_name) as f:
@@ -26,28 +33,21 @@ def read_line(file_name):
         return first_line
 
 def list_files():
-    files = glob.glob('**/*.md', recursive=True)
+    files = glob.glob(PREFIX + '**/*.md', recursive=True)
     pair = [f for f in files if '/' in f and not f.startswith('misc') ] 
     return pair
-
-def define_date_ranges():
-    start = len(year_format) + len('/')
-    end = start + len(date_format)
-    return (start, end)
-
-(date_start, date_end) = define_date_ranges()
 
 def extract_date(line):
     date =  line[date_start:date_end]
     year = date[:len(year_format)]
     return (date, year)
 
-post_by_year = defaultdict(list)
-
 def write_index_to_file(file):
+    post_by_year = defaultdict(list)
+
     for file_name in list_files():
         title = read_line(file_name)[1:-1].strip() # remove leading `# ` and strip spaces
-        (date, year) = extract_date(file_name)
+        date, year = extract_date(file_name)
         line = f"{date} - [{title}]({file_name})"
         post_by_year[year].append(line)
 
@@ -56,6 +56,10 @@ def write_index_to_file(file):
         for year_line in sorted(post_by_year[year], reverse = True):
             print(f"- {year_line}", file=file)
 
-with open(INDEX_FILE, 'w') as f:
-    f.write(README_HEADER)
-    write_index_to_file(f)
+def main():
+    with open(INDEX_FILE, 'w') as f:
+        f.write(README_HEADER)
+        write_index_to_file(f)
+
+if __name__ == "__main__":
+    main()
